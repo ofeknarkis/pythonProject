ff516cef87ff440bc04dfafd51186149b35d9e92
ofeknarkis <102508714+ofeknarkis@users.noreply.github.com>
2022-03-28 13:52:24 +0300

    hfshdfjkd

---
+++ for and while.py
@@ -0,0 +1,6 @@
+num1=input("enter your age: ")
+num2=input("enter your dick size: ")
+while num1>num2:
+    print("why are you gay?")
+num1=input("enter your age: ")
+num2=input("enter your dick size: ")
